2beac3a5a250daaf7a9ace8b7c70dfbc86226c94
HuzaifaEjazDev <huzaifaejazdev@gmail.com>
2025-11-20 12:05:21 +0500

    Initial commit with main.dart only

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'core/theme.dart';
+import 'viewmodels/player_viewmodel.dart';
+import 'viewmodels/home_viewmodel.dart';
+import 'viewmodels/library_viewmodel.dart';
+import 'views/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => PlayerViewModel()),
+        ChangeNotifierProvider(create: (_) => HomeViewModel()),
+        ChangeNotifierProvider(create: (_) => LibraryViewModel()),
+      ],
+      child: MaterialApp(
+        title: 'Vibia',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.darkTheme,
+        home: const MainScreen(),
+      ),
+    );
+  }
+}
